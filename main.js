9267108d3513120873364c91f31bbe61d86db090
Antoine-marchais <antoine.marchais@student.ecp.fr>
2018-10-25 19:03:18 +0200

    domain verified

--- main.js
+++ main.js
@@ -2,14 +2,16 @@ const express = require("express");
 const PORT = process.env.PORT;
 
 const app = express();
-
-app.get("/secret",function(req,res){
-    res.statusCode = 200;
-    res.send('coucou');
-});
+//google domain verification
 app.get("/googlecc96d596baba5bdf.html",function(req,res){
     res.statusCode = 200;
     res.send("google-site-verification: googlecc96d596baba5bdf.html");
+});
+
+app.post("/",function(req,res){
+    console.log(req);
+    res.statusCode = 200;
+    res.send("ok");
 })
 
 app.listen(PORT);
