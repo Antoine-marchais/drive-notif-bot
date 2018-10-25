1ffe3f9504830ac650ddfdfc82c234bf8ff377a3
Antoine-marchais <antoine.marchais@student.ecp.fr>
2018-10-25 18:32:31 +0200

    fix

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const PORT = process.env.PORT;
 
 const app = express();
 
-app.get("/secret",function(req,res){
-    console.log(req);
+app.get("/secret",function(res){
     res.statusCode = 200;
     res.send('coucou');
 });
